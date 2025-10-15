bc4e40e718123d1cc965fdbafc7079f74778d49d
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-10-15 18:14:12 +0800

    2025年10月15日

--- 1-原始代码/build_model/build_model.sh
+++
@@ -1,7 +0,0 @@
-# 构建突变模型以计算期望值
-
-python3 build_model/compile_denovo.py
-python3 build_model/compare_denovo.py
-python3 build_model/filter_denovo.py
-python3 build_model/composite_likelihood_mito.py
-python3 build_model/annotate_mutations.py
---
+++ 4-区域约束/1-主执行脚本.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#!/bin/bash
+#!不建议一次性运行，因为代码中有绝对路径
+#!千万不要修改python代码文件名
+#!如下路径千万不要修改，也不要重命名文件名、文件路径等:
+# ├── 0-required_files
+# │   ├── databases
+# │   ├── input_denovo
+# │   ├── insilicos
+# │   ├── other_annotations
+# │   ├── pdb
+# │   ├── svg_input
+# │   ├── synthetic_vcf
+# │   └── 阅读我.md
+# ├── 1-构建模型
+# │   ├── 1-主执行脚本.sh
+# │   ├── __pycache__
+# │   ├── annotate_mutations.py
+# │   ├── compare_denovo.py
+# │   ├── compile_denovo.py
+# │   ├── composite_likelihood_mito.py
+# │   ├── filter_denovo.py
+# │   └── 蒙特卡洛模拟异质性.R
+# ├── 1-警告.md
+# ├── 2-计算观测值和预期值
+# │   ├── 1-主执行脚本.sh
+# │   ├── __pycache__
+# │   ├── calibrate_model_step1.py
+# │   ├── calibrate_model_step2.R
+# │   ├── oe_consequences.py
+# │   ├── oe_functions.py
+# │   ├── oe_loci.py
+# │   ├── oe_lookup.py
+# │   ├── oe_other.py
+# │   └── 阅读我.md
+# ├── 3-局部约束
+# │   ├── 1-主执行脚本.sh
+# │   ├── __pycache__
+# │   ├── local_constraint.py
+# │   └── 阅读我.md
+# ├── 4-区域约束
+# │   ├── 1-主执行脚本.sh
+# │   ├── __pycache__
+# │   ├── annotate_regional_constraint.py
+# │   ├── apply_FDR_filter.py
+# │   ├── regional_constraint.py
+# │   └── 阅读我.md
+
+
+cd /mnt/f/OneDrive/文档（科研）/脚本/Download/16-Constraint-mtDNA-model
+#* ================第一步================
+python3 4-区域约束/regional_constraint.py
+#* ================第二步================
+python3 4-区域约束/annotate_regional_constraint.py
+#* ================第三步================
+python3 4-区域约束/apply_FDR_filter.py
